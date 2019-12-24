fba70d7abbbdce1580d5c3ae7ffd64fd8bd91b14
mhibatallah <45025650+mhibatallah@users.noreply.github.com>
2019-12-24 09:48:31 -0500

    Delete run_vmc.py

--- 2DTFIM_f/run_vmc.py
+++
@@ -1,3 +0,0 @@
-from RNN_wavefunction_2DTFIM_OBC_GRU import runvmc
-
-runvmc(numsteps = 2*10**4, systemsize_x = 4, systemsize_y = 4, Bx = 3, num_units = 50, num_layers = 1, num_samples = 500, learningrate = 1e-3, seed = 333)
