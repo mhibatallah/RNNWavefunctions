808a8faa90a1c8d8f18c0a60c8bf8f3f3bf1704f
mhibatallah <45025650+mhibatallah@users.noreply.github.com>
2020-01-11 16:28:13 -0500

    Update run_1dTFIM.py

--- 1DTFIM/run_1dTFIM.py
+++ 1DTFIM/run_1dTFIM.py
@@ -1,3 +1,3 @@
-from RNN_wavefunction_TFIM_GRU import run_1DTFIM
+from RNN_wavefunction_1DTFIM import run_1DTFIM
 
 run_1DTFIM(numsteps = 10**3, systemsize = 20, num_units = 50, num_layers = 1, num_samples = 500, learningrate = 5e-3, seed = 111)
