20bf60b900a15f36c0e92afa164d69111b681d26
mhibatallah <45025650+mhibatallah@users.noreply.github.com>
2020-01-11 16:50:19 -0500

    Update run_2dTFIM.py

--- 2DTFIM_1DRNN/run_2dTFIM.py
+++ 2DTFIM_1DRNN/run_2dTFIM.py
@@ -1,3 +1,3 @@
-from RNN_wavefunction_2DTFIM import run_2DTFIM
+from RNN_wavefunction_2DTFIM_1DRNN import run_2DTFIM
 
 run_2DTFIM(numsteps = 2*10**4, systemsize_x = 4, systemsize_y = 4, Bx = 3, num_units = 50, num_layers = 1, num_samples = 500, learningrate = 1e-3, seed = 333)
