329f4ef81de0b23644553bb98e08bdcd4a2d037b
mhibatallah <45025650+mhibatallah@users.noreply.github.com>
2020-01-11 22:46:04 -0500

    Update run_j1j2.py

--- J1J2/run_j1j2.py
+++ J1J2/run_j1j2.py
@@ -1,3 +1,3 @@
 from Training_J1J2 import run_J1J2
 
-run_J1J2(numsteps = 10**3, systemsize = 20, J1_  = 1.0, J2_ = 0.0, num_units = 50, num_layers = 1, num_samples = 500, learningrate = 2.5*1e-3, seed = 111)
+run_J1J2(numsteps = 10**3, systemsize = 20, J1_  = 1.0, J2_ = 0.0, num_units = 50, num_layers = 1, num_samples = 500, learningrate = 2.5*1e-4, seed = 111)
