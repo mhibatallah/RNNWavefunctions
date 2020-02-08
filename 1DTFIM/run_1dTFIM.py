1ab08df7bc7e5edc9b1b0599afbcb560b5d6431b
Mohamed Hibat Allah <45025650+mhibatallah@users.noreply.github.com>
2020-02-08 09:49:09 -0500

    Update run_1dTFIM.py

--- 1DTFIM/run_1dTFIM.py
+++ 1DTFIM/run_1dTFIM.py
@@ -1,3 +1,8 @@
 from Training_1DTFIM import run_1DTFIM
 
+#numsteps = number of training iterations
+#systemsize = number of physical spins
+#Bx = transverse magnetic field
+#numsamples = number of samples used for training
+#num_units = number of memory units of the hidden state of the RNN
 run_1DTFIM(numsteps = 10**3, systemsize = 20, Bx = +1, num_units = 50,  num_layers = 1, numsamples = 500, learningrate = 5e-3, seed = 111)
