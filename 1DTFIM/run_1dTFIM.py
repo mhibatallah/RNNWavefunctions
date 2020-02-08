c18fa6643af2f408e5c72f824735aa81dd7f4636
Mohamed Hibat Allah <45025650+mhibatallah@users.noreply.github.com>
2020-02-08 09:51:09 -0500

    Update run_1dTFIM.py

--- 1DTFIM/run_1dTFIM.py
+++ 1DTFIM/run_1dTFIM.py
@@ -5,4 +5,5 @@ from Training_1DTFIM import run_1DTFIM
 #Bx = transverse magnetic field
 #numsamples = number of samples used for training
 #num_units = number of memory units of the hidden state of the RNN
+#num_layers = number of vertically stacked RNN cells
 run_1DTFIM(numsteps = 10**3, systemsize = 20, Bx = +1, num_units = 50,  num_layers = 1, numsamples = 500, learningrate = 5e-3, seed = 111)
