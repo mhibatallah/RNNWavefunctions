3142ac997f0e7db0dee7ce0795880e4cfc17e92b
Mohamed Hibat Allah <45025650+mhibatallah@users.noreply.github.com>
2020-02-08 09:50:49 -0500

    Update run_2dTFIM.py

--- 2DTFIM_1DRNN/run_2dTFIM.py
+++ 2DTFIM_1DRNN/run_2dTFIM.py
@@ -1,3 +1,10 @@
 from Training_2DTFIM_1DRNN import run_2DTFIM
 
+#numsteps = number of training iterations
+#systemsize_x = the size of the x-dimension of the square lattice
+#systemsize_x = the size of the y-dimension of the square lattice
+#Bx = transverse magnetic field
+#numsamples = number of samples used for training
+#num_units = number of memory units of the hidden state of the RNN
+#num_layers = number of vertically stacked RNN cells
 run_2DTFIM(numsteps = 2*10**4, systemsize_x = 4, systemsize_y = 4, Bx = 3, num_units = 50, num_layers = 1, numsamples = 500, learningrate = 1e-3, seed = 333)
