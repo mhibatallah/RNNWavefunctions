ea38ee6a484f142f28390735a4271e859ada1e02
Mohamed Hibat Allah <45025650+mhibatallah@users.noreply.github.com>
2020-02-08 21:27:12 -0500

    Update run_2dTFIM.py

--- 2DTFIM_2DRNN/run_2dTFIM.py
+++ 2DTFIM_2DRNN/run_2dTFIM.py
@@ -7,4 +7,4 @@ from Training2DRNN_2DTFIM import run_2DTFIM
 #numsamples = number of samples used for training
 #num_units = number of memory units of the hidden state of the RNN
 #num_layers is not supported yet, stay tuned!
-run_2DTFIM(numsteps = 2*10**4, systemsize_x = 4, systemsize_y = 4, Bx = 3, num_units = 50, num_samples = 500, learningrate = 5e-3, seed = 111)
+meanEnergy, varEnergy = run_2DTFIM(numsteps = 2*10**4, systemsize_x = 4, systemsize_y = 4, Bx = 3, num_units = 50, num_samples = 500, learningrate = 5e-3, seed = 111)
