1c2e8b6b91a2f7325c319d1ea13912cdd2fe4eaa
Mohamed Hibat Allah <45025650+mhibatallah@users.noreply.github.com>
2020-02-09 11:13:08 -0500

    Update run_j1j2.py

--- J1J2/run_j1j2.py
+++ J1J2/run_j1j2.py
@@ -6,4 +6,9 @@ from TrainingRNN_J1J2 import run_J1J2
 #numsamples = number of samples used for training
 #num_units = number of memory units of the hidden state of the RNN
 #num_layers = number of vertically stacked RNN cells
-meanEnergy, varEnergy = run_J1J2(numsteps = 5*10**3, systemsize = 20, J1_  = 1.0, J2_ = 0.0, num_units = 50, num_layers = 1, numsamples = 500, learningrate = 2.5*1e-3, seed = 111)
+
+#This function trains a pRNN wavefunction for 1DTFIM with the corresponding hyperparams
+RNNEnergy, varRNNEnergy = run_J1J2(numsteps = 3000, systemsize = 10, J1_  = 1.0, J2_ = 0.2, Marshall_sign = False, num_units = 10, num_layers = 1, numsamples = 200, learningrate = 5e-4, seed = 111)
+
+#RNNEnergy is a numpy array of the variational energy of the pRNN wavefunction
+#varRNNEnergy is a numpy array of the variance of the variational energy of the pRNN wavefunction
