85664cf7dae500826caec76e2fd963f345259154
Mohamed Hibat Allah <45025650+mhibatallah@users.noreply.github.com>
2020-02-09 13:00:57 -0500

    Update run_1dTFIM.py

--- 1DTFIM/run_1dTFIM.py
+++ 1DTFIM/run_1dTFIM.py
@@ -6,7 +6,7 @@ from TrainingRNN_1DTFIM import run_1DTFIM
 #numsamples = number of samples used for training
 #num_units = number of memory units of the hidden state of the RNN
 #num_layers = number of vertically stacked RNN cells
-meanEnergy, varEnergy = run_1DTFIM(numsteps = 10**3, systemsize = 20, Bx = +1, num_units = 50,  num_layers = 1, numsamples = 500, learningrate = 5e-3, seed = 111)
+RNNEnergy, varRNNEnergy = run_1DTFIM(numsteps = 10**3, systemsize = 20, Bx = +1, num_units = 50,  num_layers = 1, numsamples = 500, learningrate = 5e-3, seed = 111)
 
 #RNNEnergy is a numpy array of the variational energy of the pRNN wavefunction
 #varRNNEnergy is a numpy array of the variance of the variational energy of the pRNN wavefunction
