a434b0f1db6f379f208df7a1ca3fa1d2a2c5933b
Mohamed Hibat-Allah <45025650+mhibatallah@users.noreply.github.com>
2021-04-08 19:32:46 -0400

    Update TrainingRNN_J1J2.py

--- J1J2/TrainingRNN_J1J2.py
+++ J1J2/TrainingRNN_J1J2.py
@@ -195,7 +195,7 @@ def run_J1J2(numsteps = 10**5, systemsize = 20, J1_  = 1.0, J2_ = 0.0, Marshall_
             #End calculate Gradients---------------
 
             optstep=optimizer.apply_gradients(zip(gradients,variables),global_step=global_step)
-            sess.run(tf.variables_initializer(optimizer.variables()),feed_dict={learningrate_placeholder: lr})
+            sess.run(tf.variables_initializer(optimizer.variables()))
 
             saver=tf.train.Saver() #define tf saver
 
